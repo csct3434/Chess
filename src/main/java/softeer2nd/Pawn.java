e6b5ea6b750e62475f366d1110f9f503e096ffcf
csct3434 <csct3434@gmail.com>
2023-07-04 11:39:07 +0900

    feat: 흰색 폰을 생성하는 Pawn 기본 생성자 추가

--- src/main/java/softeer2nd/Pawn.java
+++ src/main/java/softeer2nd/Pawn.java
@@ -3,6 +3,10 @@ package softeer2nd;
 public class Pawn {
     String color;
 
+    public Pawn() {
+        this.color = "white";
+    }
+
     public Pawn(String color) {
         this.color = color;
     }
