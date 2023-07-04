17925c4ea3b438d2eea98961fa29febf3a215411
csct3434 <csct3434@gmail.com>
2023-07-04 17:31:21 +0900

    refactor: representation 데이터 타입 String에서 char로 정정

--- src/main/java/chess/pieces/Pawn.java
+++ src/main/java/chess/pieces/Pawn.java
@@ -2,22 +2,32 @@ package chess.pieces;
 
 public class Pawn {
 
+    // 폰의 색상을 나타냅니다.
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
 
-    public static final String[] colors = {WHITE_COLOR, BLACK_COLOR};
+    // 폰의 출력 문자를 나타냅니다.
+    public static final char WHITE_REPRESENTATION = 'p';
+    public static final char BLACK_REPRESENTATION = 'P';
 
     private String color;
+    private char representation;
 
     public Pawn() {
         this.color = WHITE_COLOR;
+        this.representation = WHITE_REPRESENTATION;
     }
 
-    public Pawn(String color) {
+    public Pawn(String color, char representation) {
         this.color = color;
+        this.representation = representation;
     }
 
     public String getColor() {
         return color;
+    }
+
+    public char getRepresentation() {
+        return representation;
     }
 }
