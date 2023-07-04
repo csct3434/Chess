050e75d888d2f3beb7343ed9a9b548ada9625f41
csct3434 <csct3434@gmail.com>
2023-07-04 10:51:44 +0900

    feat: Pawn 클래스 구현

---
+++ src/main/java/softeer2nd/Pawn.java
@@ -0,0 +1,13 @@
+package softeer2nd;
+
+public class Pawn {
+    String color;
+
+    public Pawn(String color) {
+        this.color = color;
+    }
+
+    public String getColor() {
+        return color;
+    }
+}
