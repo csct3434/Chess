5ab68fd1f5356ad8ecf6c632ca3b4576fb205476
csct3434 <csct3434@gmail.com>
2023-07-04 12:08:38 +0900

    fix: 체스판에 폰을 추가하는 메서드를 수정(메서드 이름 및 매개변수 타입 변경)

--- src/main/java/softeer2nd/Board.java
+++ src/main/java/softeer2nd/Board.java
@@ -5,8 +5,7 @@ import java.util.ArrayList;
 public class Board {
     private ArrayList<Pawn> pawnList = new ArrayList<>();
 
-    public void addPawn(String color) {
-        Pawn newPawn = new Pawn(color);
-        pawnList.add(newPawn);
+    public void add(Pawn pawn) {
+        pawnList.add(pawn);
     }
 }
