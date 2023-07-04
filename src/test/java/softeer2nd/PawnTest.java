f5b8a93a71d4d8b3ec4d54d7dd7d9d6f2934380e
csct3434 <csct3434@gmail.com>
2023-07-04 10:54:46 +0900

    test: 검은색 폰 생성 테스트 코드 추가

--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -4,12 +4,17 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
 public class PawnTest {
 
     @Test
-    @DisplayName("흰색 폰이 생성되어야 한다")
+    @DisplayName("흰색 폰과 검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn pawn = new Pawn("white");
-        Assertions.assertThat(pawn.getColor()).isEqualTo("white");
+        Pawn whitePawn = new Pawn("white");
+        assertThat(whitePawn.getColor()).isEqualTo("white");
+
+        Pawn blackPawn = new Pawn("black");
+        assertThat(blackPawn.getColor()).isEqualTo("black");
     }
 }
