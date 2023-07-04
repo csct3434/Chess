2f298299b504471b509ed3edf83eb91b078784fe
csct3434 <csct3434@gmail.com>
2023-07-04 11:43:08 +0900

    test: Pawn 클래스 기본 생성자 테스트 코드 작성

--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -5,6 +5,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
 
@@ -23,5 +24,12 @@ public class PawnTest {
 
     private boolean verifyPawn(Pawn pawn, String color) {
         return pawn.getColor().equals(color);
+    }
+
+    @Test
+    @DisplayName("폰 색상 미지정 시, 흰색 폰이 생성되어야 한다")
+    public void create_기본생성자() throws Exception {
+        Pawn pawn = new Pawn();
+        assertEquals("white", pawn.getColor());
     }
 }
