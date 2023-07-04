97bc162d315a0a8de645d063c7194e67f95a8ff5
csct3434 <csct3434@gmail.com>
2023-07-04 10:55:28 +0900

    refactor: 폰 색상 지역변수 추가, 폰 색상 검증 메서드 verifyPawn() 추가

--- src/test/java/softeer2nd/PawnTest.java
+++ src/test/java/softeer2nd/PawnTest.java
@@ -11,10 +11,17 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰과 검은색 폰이 생성되어야 한다")
     public void create() {
-        Pawn whitePawn = new Pawn("white");
-        assertThat(whitePawn.getColor()).isEqualTo("white");
+        String colorWhite = "white";
+        String colorBlack = "black";
 
-        Pawn blackPawn = new Pawn("black");
-        assertThat(blackPawn.getColor()).isEqualTo("black");
+        Pawn whitePawn = new Pawn(colorWhite);
+        assertThat(verifyPawn(whitePawn, colorWhite)).isEqualTo(true);
+
+        Pawn blackPawn = new Pawn(colorBlack);
+        assertThat(verifyPawn(blackPawn, colorBlack)).isEqualTo(true);
+    }
+
+    private boolean verifyPawn(Pawn pawn, String color) {
+        return pawn.getColor().equals(color);
     }
 }
