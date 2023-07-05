c0130dcd8857ee0d9b4396b71ee327c82fc9023b
csct3434 <csct3434@gmail.com>
2023-07-05 22:27:50 +0900

    feat(Piece): 기물의 종류를 의미하는 name 속성 추가, 기물의 색상과 종류 별로 인스턴스를 생성하는 팩토리 메소드 추가

--- src/main/java/chess/pieces/Piece.java
+++ src/main/java/chess/pieces/Piece.java
@@ -2,24 +2,42 @@ package chess.pieces;
 
 public class Piece {
 
-    // 폰의 색상을 나타냅니다.
+    // 기물 색상 상수
     public static final String WHITE_COLOR = "white";
     public static final String BLACK_COLOR = "black";
 
-    // 폰의 출력 문자를 나타냅니다.
-    public static final char WHITE_REPRESENTATION = 'p';
-    public static final char BLACK_REPRESENTATION = 'P';
+    // 기물 종류 상수
+    public static final String PAWN = "pawn";
+    public static final String KNIGHT = "knight";
+    public static final String ROCK = "rook";
+    public static final String BISHOP = "bishop";
+    public static final String QUEEN = "queen";
+    public static final String KING = "king";
 
+    // 기물 출력 문자 상수
+    public static final char WHITE_PAWN_REPRESENTATION = 'p';
+    public static final char BLACK_PAWN_REPRESENTATION = 'P';
+    public static final char WHITE_KNIGHT_REPRESENTATION = 'n';
+    public static final char BLACK_KNIGHT_REPRESENTATION = 'N';
+    public static final char WHITE_ROCK_REPRESENTATION = 'r';
+    public static final char BLACK_ROCK_REPRESENTATION = 'R';
+    public static final char WHITE_BISHOP_REPRESENTATION = 'b';
+    public static final char BLACK_BISHOP_REPRESENTATION = 'B';
+    public static final char WHITE_QUEEN_REPRESENTATION = 'q';
+    public static final char BLACK_QUEEN_REPRESENTATION = 'Q';
+    public static final char WHITE_KING_REPRESENTATION = 'k';
+    public static final char BLACK_KING_REPRESENTATION = 'K';
+
+    // 기물의 색상
     private String color;
+    // 기물의 종류
+    private String name;
+    // 기물의 출력문자
     private char representation;
 
-    public Piece() {
-        this.color = WHITE_COLOR;
-        this.representation = WHITE_REPRESENTATION;
-    }
-
-    public Piece(String color, char representation) {
+    private Piece(String color, String name, char representation) {
         this.color = color;
+        this.name = name;
         this.representation = representation;
     }
 
@@ -27,7 +45,60 @@ public class Piece {
         return color;
     }
 
+    public String getName() {
+        return name;
+    }
+
     public char getRepresentation() {
         return representation;
     }
+
+    public static Piece createWhitePawn() {
+        return new Piece(WHITE_COLOR, PAWN, WHITE_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createBlackPawn() {
+        return new Piece(BLACK_COLOR, PAWN, BLACK_PAWN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKnight() {
+        return new Piece(WHITE_COLOR, KNIGHT, WHITE_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createBlackKnight() {
+        return new Piece(BLACK_COLOR, KNIGHT, BLACK_KNIGHT_REPRESENTATION);
+    }
+
+    public static Piece createWhiteRock() {
+        return new Piece(WHITE_COLOR, ROCK, WHITE_ROCK_REPRESENTATION);
+    }
+
+    public static Piece createBlackRock() {
+        return new Piece(BLACK_COLOR, ROCK, BLACK_ROCK_REPRESENTATION);
+    }
+
+    public static Piece createWhiteBishop() {
+        return new Piece(WHITE_COLOR, BISHOP, WHITE_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createBlackBishop() {
+        return new Piece(BLACK_COLOR, BISHOP, BLACK_BISHOP_REPRESENTATION);
+    }
+
+    public static Piece createWhiteQueen() {
+        return new Piece(WHITE_COLOR, QUEEN, WHITE_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createBlackQueen() {
+        return new Piece(BLACK_COLOR, QUEEN, BLACK_QUEEN_REPRESENTATION);
+    }
+
+    public static Piece createWhiteKing() {
+        return new Piece(WHITE_COLOR, KING, WHITE_KING_REPRESENTATION);
+    }
+
+    public static Piece createBlackKing() {
+        return new Piece(BLACK_COLOR, KING, BLACK_KING_REPRESENTATION);
+    }
+
 }
