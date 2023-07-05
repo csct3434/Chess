51d1936cb54dc0e129ff647f1ed932e8d98bdd5c
csct3434 <csct3434@gmail.com>
2023-07-05 10:33:44 +0900

    feat: Board 초기화 기능, 폰 열 결과 반환 메서드 추가

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -5,17 +5,31 @@ import chess.pieces.Pawn;
 import java.util.ArrayList;
 
 public class Board {
-    private ArrayList<Pawn> pawnList = new ArrayList<>();
 
-    public void add(Pawn pawn) {
-        pawnList.add(pawn);
+    private ArrayList<Pawn> whitePawnList = new ArrayList<>();
+    private ArrayList<Pawn> blackPawnList = new ArrayList<>();
+
+    public void initialize() {
+        for(int i=0; i<8; i++) {
+            whitePawnList.add(new Pawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION));
+            blackPawnList.add(new Pawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION));
+        }
     }
 
-    public int size() {
-        return pawnList.size();
+    public String getWhitePawnsResult() {
+        StringBuilder sb = new StringBuilder();
+        for(Pawn whitePawn : whitePawnList) {
+            sb.append(whitePawn.getRepresentation());
+        }
+        return sb.toString();
     }
 
-    public Pawn findPawn(int index) {
-        return pawnList.get(index);
+    public String getBlackPawnsResult() {
+        StringBuilder sb = new StringBuilder();
+        for(Pawn blackPawn : blackPawnList) {
+            sb.append(blackPawn.getRepresentation());
+        }
+        return sb.toString();
     }
+
 }
