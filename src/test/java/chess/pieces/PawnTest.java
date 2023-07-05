ef255c3f5f8b528aea549c5970f4d53d3a342e0c
csct3434 <csct3434@gmail.com>
2023-07-05 10:28:49 +0900

    refactor: PawnTest 리팩토링

--- src/test/java/chess/pieces/PawnTest.java
+++ src/test/java/chess/pieces/PawnTest.java
@@ -3,7 +3,6 @@ package chess.pieces;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class PawnTest {
@@ -11,14 +10,14 @@ public class PawnTest {
     @Test
     @DisplayName("흰색 폰과 검은색 폰이 생성되어야 한다")
     public void create() {
-        for(String pawnColor : Pawn.colors) {
-            Pawn newPawn = new Pawn(pawnColor);
-            assertThat(verifyPawn(newPawn, pawnColor)).isEqualTo(true);
-        }
+        verifyPawn(Pawn.WHITE_COLOR, Pawn.WHITE_REPRESENTATION);
+        verifyPawn(Pawn.BLACK_COLOR, Pawn.BLACK_REPRESENTATION);
     }
 
-    private boolean verifyPawn(Pawn pawn, String color) {
-        return pawn.getColor().equals(color);
+    void verifyPawn(final String color, final char representation) {
+        Pawn pawn = new Pawn(color, representation);
+        assertEquals(color, pawn.getColor());
+        assertEquals(representation, pawn.getRepresentation());
     }
 
     @Test
@@ -26,5 +25,6 @@ public class PawnTest {
     public void create_기본생성자() throws Exception {
         Pawn pawn = new Pawn();
         assertEquals(Pawn.WHITE_COLOR, pawn.getColor());
+        assertEquals(Pawn.WHITE_REPRESENTATION, pawn.getRepresentation());
     }
 }
