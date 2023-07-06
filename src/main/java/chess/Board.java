cb5c07891fb5a8499e364f7e962077527f33f80d
csct3434 <csct3434@gmail.com>
2023-07-06 11:13:24 +0900

    feat(Board): 체스판 전체 기물의 상태를 보여주는 showBoard() 메서드 구현

--- src/main/java/chess/Board.java
+++ src/main/java/chess/Board.java
@@ -1,59 +1,115 @@
 package chess;
 
 import chess.pieces.Piece;
+import utils.StringUtils;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import static utils.StringUtils.appendNewLine;
 
 public class Board {
 
-    private ArrayList<Piece> whitePieceList = new ArrayList<>();
-    private ArrayList<Piece> blackPieceList = new ArrayList<>();
+    public static final int FILES = 8;
+    public static final int RANK = 8;
+
+    private List<Piece> whitePieces;
+    private List<Piece> blackPieces;
 
     public void initialize() {
-        for(int i=0; i<8; i++) {
-            whitePieceList.add(new Piece(Piece.WHITE_COLOR, Piece.WHITE_REPRESENTATION));
-            blackPieceList.add(new Piece(Piece.BLACK_COLOR, Piece.BLACK_REPRESENTATION));
+        whitePieces = new ArrayList<>();
+        blackPieces = new ArrayList<>();
+
+        createWhitePieces();
+        createBlackPieces();
+    }
+
+    private void createWhitePieces() {
+        createWhitePawns();
+        createWhiteMajorPieces();
+    }
+
+    private void createBlackPieces() {
+        createBlackMajorPieces();
+        createBlackPawns();
+    }
+
+    private void createWhitePawns() {
+        for (int i = 0; i < FILES; i++) {
+            whitePieces.add(Piece.createWhitePawn());
         }
     }
 
-    public String getWhitePawnsResult() {
-        return getPawnsResult(Piece.WHITE_COLOR);
+    private void createBlackPawns() {
+        for (int i = 0; i < FILES; i++) {
+            blackPieces.add(Piece.createBlackPawn());
+        }
     }
 
-    public String getBlackPawnsResult() {
-        return getPawnsResult(Piece.BLACK_COLOR);
+    private void createWhiteMajorPieces() {
+        whitePieces.add(Piece.createWhiteRock());
+        whitePieces.add(Piece.createWhiteKnight());
+        whitePieces.add(Piece.createWhiteBishop());
+        whitePieces.add(Piece.createWhiteQueen());
+        whitePieces.add(Piece.createWhiteKing());
+        whitePieces.add(Piece.createWhiteBishop());
+        whitePieces.add(Piece.createWhiteKnight());
+        whitePieces.add(Piece.createWhiteRock());
     }
 
-    private String getPawnsResult(String color) {
+    private void createBlackMajorPieces() {
+        blackPieces.add(Piece.createBlackRock());
+        blackPieces.add(Piece.createBlackKnight());
+        blackPieces.add(Piece.createBlackBishop());
+        blackPieces.add(Piece.createBlackQueen());
+        blackPieces.add(Piece.createBlackKing());
+        blackPieces.add(Piece.createBlackBishop());
+        blackPieces.add(Piece.createBlackKnight());
+        blackPieces.add(Piece.createBlackRock());
+    }
+
+    public int pieceCount() {
+        return whitePieces.size() + blackPieces.size();
+    }
+
+    public String showBoard() {
         StringBuilder sb = new StringBuilder();
+        String blankRankResult = appendNewLine("........");
 
-        ArrayList<Piece> pieceList;
-
-        if(color.equals(Piece.WHITE_COLOR)) {
-            pieceList = whitePieceList;
-        } else {
-            pieceList = blackPieceList;
-        }
-
-        for(Piece piece : pieceList) {
-            sb.append(piece.getRepresentation());
-        }
+        sb.append(getBlackPiecesResult())
+                .append(blankRankResult)
+                .append(blankRankResult)
+                .append(blankRankResult)
+                .append(blankRankResult)
+                .append(getWhitePiecesResult());
 
         return sb.toString();
     }
 
-    public String print() {
+    private String getWhitePiecesResult() {
         StringBuilder sb = new StringBuilder();
 
-        sb.append("........\n");
-        sb.append(getBlackPawnsResult() + '\n');
-        sb.append("........\n");
-        sb.append("........\n");
-        sb.append("........\n");
-        sb.append("........\n");
-        sb.append(getWhitePawnsResult() + '\n');
-        sb.append("........\n");
+        for (int i = 0; i < whitePieces.size(); i++) {
+            sb.append(whitePieces.get(i).getRepresentation());
+            if (i == 7) {
+                sb.append(StringUtils.NEWLINE);
+            }
+        }
 
-        return sb.toString();
+        return appendNewLine(sb.toString());
     }
+
+    private String getBlackPiecesResult() {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < blackPieces.size(); i++) {
+            sb.append(blackPieces.get(i).getRepresentation());
+            if (i == 7) {
+                sb.append(StringUtils.NEWLINE);
+            }
+        }
+
+        return appendNewLine(sb.toString());
+    }
+
 }
