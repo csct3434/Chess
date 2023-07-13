5f738115b8aad8b4bee351160d2211d4f766a145
csct3434 <csct3434@gmail.com>
2023-07-13 14:48:22 +0900

    refactor(Main) : Main에서의 화면 출력 코드를 ChessView로 이동

--- src/main/java/chess/ChessView.java
+++ src/main/java/chess/ChessView.java
@@ -10,9 +10,19 @@ public class ChessView {
         this.board = board;
     }
 
+    public void showPrompt() {
+        System.out.print("userInput: ");
+    }
+
+    public void showMessage(String message) {
+        System.out.println("message = " + message);
+    }
+
     public String showBoard() {
+        System.out.println("\n[현재 체스판]");
         String boardRepresentation = board.getBoardRepresentation();
         System.out.println(boardRepresentation);
         return boardRepresentation;
     }
+
 }
--- src/main/java/chess/Main.java
+++ src/main/java/chess/Main.java
@@ -16,21 +16,6 @@ public class Main {
         run();
     }
 
-    public static void run() {
-        while (true) {
-            String userInput = getUserInput();
-
-            if (userInput.equals("start")) {
-                start();
-            } else if (userInput.equals("move")) {
-                move(userInput);
-            } else if (userInput.equals("end")) {
-                end();
-                break;
-            }
-        }
-    }
-
     private static void init() {
         Board board = new Board();
         scanner = new Scanner(System.in);
@@ -39,15 +24,26 @@ public class Main {
         gameStart = false;
     }
 
-    private static String getUserInput() {
-        System.out.print("userInput: ");
-        return scanner.nextLine().trim();
+    public static void run() {
+        while (true) {
+            chessView.showPrompt();
+            String userInput = scanner.nextLine().trim();
+
+            if (userInput.equals("start")) {
+                start();
+            } else if (userInput.equals("move")) {
+                move(userInput);
+            } else if (userInput.equals("end")) {
+                chessView.showMessage("게임을 종료합니다");
+                break;
+            }
+        }
     }
 
     private static void start() {
-        System.out.println("게임을 시작합니다.");
+        chessView.showMessage("게임을 시작합니다.");
         chessGame.initializeBoard();
-        printBoard();
+        chessView.showBoard();
         gameStart = true;
     }
 
@@ -62,13 +58,13 @@ public class Main {
             String targetSquare = arguments[2];
             chessGame.move(sourceSquare, targetSquare);
         } catch (RuntimeException exception) {
-            System.out.println(exception.getMessage());
+            chessView.showMessage(exception.getMessage());
         } finally {
-            printBoard();
+            chessView.showBoard();
         }
     }
 
-    private static void verifyMoveArguments(String[] arguments) throws IllegalArgumentException {
+    private static void verifyMoveArguments(String[] arguments) {
         if(arguments.length != 3 || arguments[1].length() != 2 || arguments[2].length() != 2) {
             throw new IllegalArgumentException("에러 : 잘못된 move 입력");
         }
@@ -78,14 +74,5 @@ public class Main {
         if(!gameStart) {
             throw new IllegalArgumentException("게임이 시작되지 않았습니다 : start 입력");
         }
-    }
-
-    private static void end() {
-        System.out.println("게임을 종료합니다.");
-    }
-
-    private static void printBoard() {
-        System.out.println("\n[현재 체스판]");
-        chessView.showBoard();
     }
 }
