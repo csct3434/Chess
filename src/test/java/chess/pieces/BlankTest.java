b41a7b600084b28743e1b20635f3ef3e74ab64cf
csct3434 <csct3434@gmail.com>
2023-07-13 20:50:54 +0900

    test(BlankTest): 빈 기물 이동 테스트 수정

--- src/test/java/chess/pieces/BlankTest.java
+++ src/test/java/chess/pieces/BlankTest.java
@@ -1,5 +1,6 @@
 package chess.pieces;
 
+import chess.board.Board;
 import chess.board.Position;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -8,11 +9,14 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class BlankTest {
     @Test
-    @DisplayName("빈 기물은 어떤 방향으로도 이동할 수 없다")
+    @DisplayName("빈 기물은 체스판의 어떤 위치로도 이동할 수 없다")
     void test() {
-        Blank blank = Blank.create(new Position("d4"));
-        for(Direction direction : Direction.everyDirection()) {
-            assertFalse(blank.verifyMovePosition(blank.getPosition().getMovedPosition(direction.getXDegree(), direction.getYDegree())));
+        Blank blank = Blank.create(new Position("a1"));
+
+        for(int xDegree = 0; xDegree < Board.LENGTH; xDegree++) {
+            for(int yDegree = 0; yDegree < Board.LENGTH; yDegree++) {
+                assertFalse(blank.verifyMovePosition(blank.getPosition().getMovedPosition(xDegree, yDegree)));
+            }
         }
     }
 }
